721786700ef0ac48ed535dd305ab89da346c8602
Timur I <nexi43048@gmail.com>
2026-09-13 12:06:38 +0500

    chore(brew): update formula to v0.1.2

--- Formula/aerofi.rb
+++ Formula/aerofi.rb
@@ -1,8 +1,8 @@
 class Aerofi < Formula
   desc "Lightweight, keyboard- and mouse-driven script launcher for macOS"
   homepage "https://github.com/frostymur/aerofi"
-  url "https://github.com/frostymur/aerofi/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "28b46202104f1a1cdae3dd32a6cae5673de2a5efcc11af2b06f407524b83ebe0"
+  url "https://github.com/frostymur/aerofi/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "6c881a0d2c5ce1232df364f374000e2b2347cfc5419d4e1155c743d083b84852"
   license "MIT"
   head "https://github.com/frostymur/aerofi.git", branch: "main"
 
