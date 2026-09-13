ecb34d47394d7a31557c99c1989fcf10a442d1f2
Timur I <nexi43048@gmail.com>
2026-09-13 07:43:40 +0500

    fix(formula): update sha256 checksum for v0.1.0 release

--- Formula/aerofi.rb
+++ Formula/aerofi.rb
@@ -2,7 +2,7 @@ class Aerofi < Formula
   desc "Lightweight, keyboard- and mouse-driven script launcher for macOS"
   homepage "https://github.com/frostymur/aerofi"
   url "https://github.com/frostymur/aerofi/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "190a8e4c3ffc3ab70a8108c4e514034e0e53498c61a64f05362747069377e3fc"
+  sha256 "28b46202104f1a1cdae3dd32a6cae5673de2a5efcc11af2b06f407524b83ebe0"
   license "MIT"
   head "https://github.com/frostymur/aerofi.git", branch: "main"
 
