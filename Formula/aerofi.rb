b649b32e78db2ee7e786168a033eec6fbf4a6090
Timur I <nexi43048@gmail.com>
2026-09-13 05:36:48 +0500

    ci(core): add GitHub Actions workflow for Cargo and Homebrew
    Signed-off-by: Timur I <nexi43048@gmail.com>

---
+++ Formula/aerofi.rb
@@ -0,0 +1,19 @@
+class Aerofi < Formula
+  desc "Lightweight, keyboard- and mouse-driven script launcher for macOS"
+  homepage "https://github.com/frostymur/aerofi"
+  url "https://github.com/frostymur/aerofi/archive/refs/tags/v0.0.7.tar.gz"
+  sha256 "190a8e4c3ffc3ab70a8108c4e514034e0e53498c61a64f05362747069377e3fc"
+  license "MIT"
+  head "https://github.com/frostymur/aerofi.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on :macos
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_path_exists bin/"aerofi"
+  end
+end
